717cb1b6ed888d2e95f3ea9f9129f64cdd3063a7
Pavel <pamitrovel@gmail.com>
2024-10-31 20:50:46 +0300

    refactor OUTPUT_ERROR_EMPTY_PASSWORD_XPATH

--- src/test/java/by/ITAcademy/MPA/LoginXpath.java
+++ src/test/java/by/ITAcademy/MPA/LoginXpath.java
@@ -8,7 +8,7 @@ public class LoginXpath {
     public static final String BUTTON_LOGIN_XPATH = "//button[@class='auth-button auth-button_primary auth-button_middle auth-form__button auth-form__button_width_full']";
 
     public static final String OUTPUT_ERROR_EMPTY_NICK_OR_EMAIL_XPATH = "//*[@id=\"auth-container\"]/div/div[2]/div/form/div[1]/div/div[2]/div/div/div[2]/div";
-    public static final String OUTPUT_ERROR_EMPTY_PASSWORD_XPATH = "//*[@id=\"auth-container\"]/div/div[2]/div/form/div[1]/div/div[2]/div/div/div[2]/div";
+    public static final String OUTPUT_ERROR_EMPTY_PASSWORD_XPATH = "//*[@id=\"auth-container\"]/div/div[2]/div/form/div[2]/div/div/div[2]/div";
     public static final String OUTPUT_ERROR_INVALID_CREDENTIALS_XPATH = "//div[@class='auth-form__description auth-form__description_error auth-form__description_base auth-form__description_extended-other']";
 
 }
