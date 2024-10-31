fc07b537fbc82debc1b3ff785986143b717035d3
Pavel <pamitrovel@gmail.com>
2024-10-31 20:11:08 +0300

    add LoginMessage

---
+++ src/test/java/by/ITAcademy/MPA/LoginMessage.java
@@ -0,0 +1,7 @@
+package by.ITAcademy.MPA;
+
+public class LoginMessage {
+    public static final String EMPTY_NICK_OR_EMAIL = "Укажите ник или e-mail";
+    public static final String EMPTY_PASSWORD = "Укажите пароль";
+    public static final String INVALID_CREDENTIALS = "Неверный логин или пароль";
+}
