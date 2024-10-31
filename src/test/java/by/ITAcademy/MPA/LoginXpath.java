8bc81bfdd14b93d19f81d932a18a8fd3e007c379
Pavel <pamitrovel@gmail.com>
2024-10-31 20:25:16 +0300

    add BUTTON_ENTER_LOGIN_PAGE_XPATH

--- src/test/java/by/ITAcademy/MPA/LoginXpath.java
+++ src/test/java/by/ITAcademy/MPA/LoginXpath.java
@@ -1,6 +1,8 @@
 package by.ITAcademy.MPA;
 
 public class LoginXpath {
+    public static final String BUTTON_ENTER_LOGIN_PAGE_XPATH = "//*[@id=\"userbar\"]/div[2]/div/div/div[1]";
+
     public static final String INPUT_NICK_OR_EMAIL_XPATH = "//*[@id=\"auth-container\"]/div/div[2]/div/form/div[1]/div/div[2]/div/div/div/div/input";
     public static final String INPUT_PASSWORD_XPATH = "//*[@id=\"auth-container\"]/div/div[2]/div/form/div[2]/div/div/div/div/input";
     public static final String BUTTON_LOGIN_XPATH = "//button[@class='auth-button auth-button_primary auth-button_middle auth-form__button auth-form__button_width_full']";
