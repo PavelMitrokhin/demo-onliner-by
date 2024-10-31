6195a2fb575f39175f6ce4e45ebd64c3568eee24
Pavel <pamitrovel@gmail.com>
2024-10-31 21:15:14 +0300

    refactor BUTTON_SHOW_PASSWORD to BUTTON_SHOW_PASSWORD_XPATH

--- src/test/java/by/ITAcademy/MPA/LoginXpath.java
+++ src/test/java/by/ITAcademy/MPA/LoginXpath.java
@@ -6,7 +6,7 @@ public class LoginXpath {
     public static final String INPUT_NICK_OR_EMAIL_XPATH = "//*[@id=\"auth-container\"]/div/div[2]/div/form/div[1]/div/div[2]/div/div/div/div/input";
     public static final String INPUT_PASSWORD_XPATH = "//*[@id=\"auth-container\"]/div/div[2]/div/form/div[2]/div/div/div/div/input";
     public static final String BUTTON_LOGIN_XPATH = "//button[@class='auth-button auth-button_primary auth-button_middle auth-form__button auth-form__button_width_full']";
-    public static final String BUTTON_SHOW_PASSWORD = "//*[@id=\"auth-container\"]/div/div[2]/div/form/div[2]/div/div/div/div/div";
+    public static final String BUTTON_SHOW_PASSWORD_XPATH = "//*[@id=\"auth-container\"]/div/div[2]/div/form/div[2]/div/div/div/div/div";
 
     public static final String OUTPUT_ERROR_EMPTY_NICK_OR_EMAIL_XPATH = "//*[@id=\"auth-container\"]/div/div[2]/div/form/div[1]/div/div[2]/div/div/div[2]/div";
     public static final String OUTPUT_ERROR_EMPTY_PASSWORD_XPATH = "//*[@id=\"auth-container\"]/div/div[2]/div/form/div[2]/div/div/div[2]/div";
